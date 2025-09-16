fb5d1a2f43ddb568a46767f648bfc0cff682b9c9
Mohamed Alaa <mohaamer1468@gmail.com>
2025-09-17 00:17:31 +0300

    adding setters , getters, constructors for entities instead of lombok package

--- src/main/java/com/movieapp/movieapp/entity/Movie.java
+++ src/main/java/com/movieapp/movieapp/entity/Movie.java
@@ -1,15 +1,11 @@
 package com.movieapp.movieapp.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
 
 @Entity
 @Table(name = "movies")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Movie {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -27,12 +23,154 @@ public class Movie {
     private String type;
 
     private String posterUrl;
-    
+
     private String genre;
-    
+
     private String director;
-    
+
     private String actors;
-    
+
     private String plot;
+
+    // ---------------- Constructors ----------------
+
+    public Movie() {
+    }
+
+    public Movie(Long id, String title, String year, String imdbId, String type,
+                 String posterUrl, String genre, String director,
+                 String actors, String plot) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.imdbId = imdbId;
+        this.type = type;
+        this.posterUrl = posterUrl;
+        this.genre = genre;
+        this.director = director;
+        this.actors = actors;
+        this.plot = plot;
+    }
+
+    // ---------------- Getters and Setters ----------------
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getYear() { return year; }
+    public void setYear(String year) { this.year = year; }
+
+    public String getImdbId() { return imdbId; }
+    public void setImdbId(String imdbId) { this.imdbId = imdbId; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    public String getPosterUrl() { return posterUrl; }
+    public void setPosterUrl(String posterUrl) { this.posterUrl = posterUrl; }
+
+    public String getGenre() { return genre; }
+    public void setGenre(String genre) { this.genre = genre; }
+
+    public String getDirector() { return director; }
+    public void setDirector(String director) { this.director = director; }
+
+    public String getActors() { return actors; }
+    public void setActors(String actors) { this.actors = actors; }
+
+    public String getPlot() { return plot; }
+    public void setPlot(String plot) { this.plot = plot; }
+
+    // ---------------- toString ----------------
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", year='" + year + '\'' +
+                ", imdbId='" + imdbId + '\'' +
+                ", type='" + type + '\'' +
+                ", posterUrl='" + posterUrl + '\'' +
+                ", genre='" + genre + '\'' +
+                ", director='" + director + '\'' +
+                ", actors='" + actors + '\'' +
+                ", plot='" + plot + '\'' +
+                '}';
+    }
+
+    // ---------------- Builder ----------------
+    public static class Builder {
+        private Long id;
+        private String title;
+        private String year;
+        private String imdbId;
+        private String type;
+        private String posterUrl;
+        private String genre;
+        private String director;
+        private String actors;
+        private String plot;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder year(String year) {
+            this.year = year;
+            return this;
+        }
+
+        public Builder imdbId(String imdbId) {
+            this.imdbId = imdbId;
+            return this;
+        }
+
+        public Builder type(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder posterUrl(String posterUrl) {
+            this.posterUrl = posterUrl;
+            return this;
+        }
+
+        public Builder genre(String genre) {
+            this.genre = genre;
+            return this;
+        }
+
+        public Builder director(String director) {
+            this.director = director;
+            return this;
+        }
+
+        public Builder actors(String actors) {
+            this.actors = actors;
+            return this;
+        }
+
+        public Builder plot(String plot) {
+            this.plot = plot;
+            return this;
+        }
+
+        public Movie build() {
+            return new Movie(id, title, year, imdbId, type, posterUrl, genre, director, actors, plot);
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
 }
--- src/main/java/com/movieapp/movieapp/entity/Rating.java
+++ src/main/java/com/movieapp/movieapp/entity/Rating.java
@@ -1,15 +1,14 @@
 package com.movieapp.movieapp.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
 
 @Entity
-@Table(name = "ratings", uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "movie_id"}))
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@Table(
+        name = "ratings",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "movie_id"})
+)
 public class Rating {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -24,4 +23,79 @@ public class Rating {
 
     @Column(nullable = false)
     private int rating; // 1-10
+
+    // ---------------- Constructors ----------------
+
+    public Rating() {
+    }
+
+    public Rating(Long id, User user, Movie movie, int rating) {
+        this.id = id;
+        this.user = user;
+        this.movie = movie;
+        this.rating = rating;
+    }
+
+    // ---------------- Getters and Setters ----------------
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+
+    public Movie getMovie() { return movie; }
+    public void setMovie(Movie movie) { this.movie = movie; }
+
+    public int getRating() { return rating; }
+    public void setRating(int rating) { this.rating = rating; }
+
+    // ---------------- toString ----------------
+
+    @Override
+    public String toString() {
+        return "Rating{" +
+                "id=" + id +
+                ", user=" + (user != null ? user.getId() : null) +
+                ", movie=" + (movie != null ? movie.getId() : null) +
+                ", rating=" + rating +
+                '}';
+    }
+
+    // ---------------- Builder ----------------
+
+    public static class Builder {
+        private Long id;
+        private User user;
+        private Movie movie;
+        private int rating;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder user(User user) {
+            this.user = user;
+            return this;
+        }
+
+        public Builder movie(Movie movie) {
+            this.movie = movie;
+            return this;
+        }
+
+        public Builder rating(int rating) {
+            this.rating = rating;
+            return this;
+        }
+
+        public Rating build() {
+            return new Rating(id, user, movie, rating);
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
 }
