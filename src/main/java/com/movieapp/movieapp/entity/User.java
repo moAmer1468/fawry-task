d2efb765f9ee8eaba86401bbc656a724f823a602
Mohamed Alaa <mohaamer1468@gmail.com>
2025-09-17 00:30:21 +0300

    we have used  lombok instead of setters , getters ...

--- src/main/java/com/movieapp/movieapp/entity/User.java
+++ src/main/java/com/movieapp/movieapp/entity/User.java
@@ -1,11 +1,15 @@
 package com.movieapp.movieapp.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -19,78 +23,4 @@ public class User {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
-
-    // ---------------- Constructors ----------------
-
-    public User() {
-    }
-
-    public User(Long id, String username, String password, Role role) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.role = role;
-    }
-
-    // ---------------- Getters and Setters ----------------
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-
-    public Role getRole() { return role; }
-    public void setRole(Role role) { this.role = role; }
-
-    // ---------------- toString ----------------
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", role=" + role +
-                '}';
-    }
-
-    // ---------------- Builder ----------------
-
-    public static class Builder {
-        private Long id;
-        private String username;
-        private String password;
-        private Role role;
-
-        public Builder id(Long id) {
-            this.id = id;
-            return this;
-        }
-
-        public Builder username(String username) {
-            this.username = username;
-            return this;
-        }
-
-        public Builder password(String password) {
-            this.password = password;
-            return this;
-        }
-
-        public Builder role(Role role) {
-            this.role = role;
-            return this;
-        }
-
-        public User build() {
-            return new User(id, username, password, role);
-        }
-    }
-
-    public static Builder builder() {
-        return new Builder();
-    }
 }
